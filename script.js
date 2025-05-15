4357ab4da1e5e234115588c4ec9ed99fb50b3681
Abhishek Airan <83405230+abhishekairan@users.noreply.github.com>
2025-05-15 23:19:11 +0530

    created a submission

--- script.js
+++ script.js
@@ -2,6 +2,19 @@ const arr = [1, 2, 3, 4, 1, 0, 2, 2];
 
 const divide = (arr, n) => {
   // Write your code here
+	const result = []
+	let subresult = []
+	let sum = 0
+	for (let a of arr) {
+		if(sum>=n){
+			result.push(subresult)
+			subresult = []
+			sum=0
+		}
+		subresult.push(a)
+		sum += a
+	}
+  return result
 };
 
 const n = prompt("Enter n: ");
